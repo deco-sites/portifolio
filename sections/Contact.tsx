4053a38eb89de700fd2221fafd04a554e1071109
fcothiago <f.t.s.goncalves@gmail.com>
2022-12-29 17:07:50 -0300

    Esboço final do portifólio

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -1,26 +1,35 @@
 import type { Image as LiveImage } from "$live/std/ui/types/Image.ts";
 
-export interface Contact{
-    icon:LiveImage;
-    title:string;
-    link:string;
-    subtitle:string;
+export interface Contact {
+  icon: LiveImage;
+  title: string;
+  link: string;
+  subtitle: string;
 }
 export interface Props {
-    contacts: Contact[];
-  }
-  export default function ProjectsComponent(props: Props) {
-    return (
-        <section id="contact" class="grid grid-cols-2 justify-items-center bg-gray-50 pb-10">
-            <h1 class="text-gray-900  text-2xl font-extrabold mt-8 text-center col-span-2">
-                Contato
-            </h1>
-            {props.contacts.map((item)=>(
-                    <div class="mt-8">
-                        <a href={item.link}><img class="mr-auto ml-auto w-24" src={item.icon} alt={item.title}/></a>
-                        <h3 class="text-lg text-gray-900 text-center">{item.title}</h3>
-                    </div>
-            ))}
-        </section>
-    )
+  contacts: Contact[];
+}
+export default function ProjectsComponent(props: Props) {
+  return (
+    <section
+      id="contact"
+      class="grid grid-cols-2 justify-items-center bg-gray-50 pb-10"
+    >
+      <h1 class="text-gray-900  text-2xl font-extrabold mt-8 text-center col-span-2">
+        Contato
+      </h1>
+      {props.contacts.map((item) => (
+        <div class="mt-8">
+          <a href={item.link}>
+            <img
+              class="mr-auto ml-auto w-24"
+              src={item.icon}
+              alt={item.title}
+            />
+          </a>
+          <h3 class="text-lg text-gray-900 text-center">{item.title}</h3>
+        </div>
+      ))}
+    </section>
+  );
 }
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,19 +1,51 @@
-export interface Props{
-    tmp:string;
+export interface Props {
+  tmp: string;
 }
 
 export default function FooterComponent(props: Props) {
-    return (
-        <footer id="footer" class="w-full bg-gray-900 grid grid-cols-1 md:grid-cols-2">
-            <div class="mt-4 ml-4 mb-10">
-                <p class="text-white text-xs mt-4 font-mono ">Icons by flaticon</p>
-                <ul>
-                    <li ><a class="text-white text-xs font-mono " href="https://www.flaticon.com/br/icones-gratis/enviar" title="enviar ícones">Enviar ícones criados por Freepik - Flaticon</a></li>
-                    <li class="text-white text-xs font-mono "><a href="https://www.flaticon.com/free-icons/telegram" title="telegram icons">Telegram icons created by Pixel perfect - Flaticon</a></li>
-                    <li class="text-white text-xs font-mono "><a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by Freepik - Flaticon</a></li>
-                    <li class="text-white text-xs font-mono "><a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by pictogramer - Flaticon</a></li>
-                </ul>
-            </div>
-        </footer>
-    );
+  return (
+    <footer
+      id="footer"
+      class="w-full bg-gray-900 grid grid-cols-1 md:grid-cols-2"
+    >
+      <div class="mt-4 ml-4 mb-10">
+        <p class="text-white text-xs mt-4 font-mono ">Icons by flaticon</p>
+        <ul>
+          <li>
+            <a
+              class="text-white text-xs font-mono "
+              href="https://www.flaticon.com/br/icones-gratis/enviar"
+              title="enviar ícones"
+            >
+              Enviar ícones criados por Freepik - Flaticon
+            </a>
+          </li>
+          <li class="text-white text-xs font-mono ">
+            <a
+              href="https://www.flaticon.com/free-icons/telegram"
+              title="telegram icons"
+            >
+              Telegram icons created by Pixel perfect - Flaticon
+            </a>
+          </li>
+          <li class="text-white text-xs font-mono ">
+            <a
+              href="https://www.flaticon.com/free-icons/linkedin"
+              title="linkedin icons"
+            >
+              Linkedin icons created by Freepik - Flaticon
+            </a>
+          </li>
+          <li class="text-white text-xs font-mono ">
+            <a
+              href="https://www.flaticon.com/free-icons/github"
+              title="github icons"
+            >
+              Github icons created by pictogramer - Flaticon
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
