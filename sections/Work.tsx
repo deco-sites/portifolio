dc66df3aae3d2aa94b48c0aedf098ca4f54a90f6
fcothiago <f.t.s.goncalves@gmail.com>
2022-12-29 06:49:35 -0300

    Experiência Profissional Iniciada

---
+++ sections/Work.tsx
@@ -0,0 +1,21 @@
+export interface Props{
+    Dummy:string;
+}
+
+export default function WorkComponent(prop:Props){
+    return(
+        <section id="work" class="grid grid-cols-1 md:grid-cols-2 bg-purple-900 justify-items-center">
+            <div class="md:col-span-2 mt-8">
+                <h1 class="text-white text-2xl font-extrabold">Experiências Profissionais</h1>
+            </div>
+            <div class="mt-8">
+                <h2 class="text-white">UFC Campus Quixadá</h2>
+            </div>
+            <div class="mt-8">
+                <h2 class="text-white">
+                    Descrição
+                </h2>
+            </div>
+        </section>
+    );
+}
